01c086eb5638190643f13c1b8e611fcc7a457f08
Yishen Miao <mys721tx@gmail.com>
2021-01-01 11:25:31 -0800

    Adding file system implementation
    Adds file system implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
+use std::ffi::{CStr, CString, OsStr, OsString};
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
 
 use clap::{app_from_crate, crate_authors, crate_description, crate_name, crate_version, Arg};
+
+use fuse_mt::*;
+
+pub struct JsonFS {
+    pub json: serde_json::Value,
+}
+
+impl FilesystemMT for JsonFS {
+    fn init(&self, _req: RequestInfo) -> ResultEmpty {
+        debug!("init");
+        Ok(())
+    }
+
+    fn destroy(&self, _req: RequestInfo) {
+        debug!("destroy");
+    }
+
+    fn getattr(&self, _req: RequestInfo, path: &Path, fh: Option<u64>) -> ResultEntry {
+        debug!("getattr: {:?}", path);
+    }
+}
 
 fn main() {
     let matches = app_from_crate!()
